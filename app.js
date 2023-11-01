105227bb4d85bbb77219826ce767481ac35056dd
abrisgilvesy <abris.gilvesy@gmail.com>
2023-11-01 17:46:40 -0400

    made dots appear

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ var h = window.innerHeight;
 // dynamically allocate svg width and height
 d3.select("#content svg").attr("width", w).attr("height", h);
 
+let projection;
+let geoGenerator;
+
+function updateGlobals(mtl) {
+  projection = d3.geoMercator().fitSize([w, h], mtl);
+  geoGenerator = d3.geoPath().projection(projection);
+}
+
 function updateMap(mtl) {
-  let projection = d3.geoMercator().fitSize([w, h], mtl);
-  let geoGenerator = d3.geoPath().projection(projection);
   let context = d3
     .select("#content g.map")
     .selectAll("path")
@@ -16,16 +22,9 @@ function updateMap(mtl) {
   context.join("path").attr("d", geoGenerator);
 }
 
-function createTestCircle() {
-  let svg = d3.select("#stops g.circles").append("circle");
-  svg.attr("cx", 100).attr("cy", 100).attr("fill", "yellow").attr("r", 50);
-}
-
-function updateStops(mtl, stops) {
-  let projection = d3.geoMercator().fitSize([w, h], mtl);
-  let geoGenerator = d3.geoPath().projection(projection);
+function updateStops(stops) {
   let svg = d3
-    .select("#stops g.circles")
+    .select("#content g.circles")
     .selectAll("circle")
     .data(stops.features)
     .enter()
@@ -38,19 +37,18 @@ function updateStops(mtl, stops) {
     .attr("cy", function (d) {
       return projection(d.geometry.coordinates)[1];
     })
-    .attr("fill", "yellow")
+    .attr("fill", "red")
     .style("opacity", 1) // Adjust the opacity as needed
     .attr("r", 4);
 }
 
 Promise.all([d3.json("data/geobase.json"), d3.json("stm.geojson")]).then(
   function (data) {
-    console.log(data[0]);
     mtl = data[0];
-    console.log(data[1]);
     stops = data[1];
+
+    updateGlobals(mtl);
     updateMap(mtl);
-    //updateStops(mtl, stops);
-    createTestCircle();
+    updateStops(stops);
   }
 );
