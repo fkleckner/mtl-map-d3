18357ad9fb879723ebf1227e6d4db189bf99aaee
Finn Kleckner <finn.kleckner@gmail.com>
2023-11-01 18:34:54 -0400

    Added stm lines

--- app.js
+++ app.js
@@ -1,44 +1,40 @@
-function updateMap(mtl) {
+function updateMap(mtl, stops, lines) {
   let projection = d3.geoMercator().fitSize([2400, 1200], mtl);
   let geoGenerator = d3.geoPath().projection(projection);
-  let context = d3
-    .select("#content g.map")
-    .selectAll("path")
-    .data(mtl.features);
 
-  context.join("path").attr("d", geoGenerator);
-}
+  let map = d3.select("#content g.map").selectAll("path").data(mtl.features);
+  map.join("path").attr("d", geoGenerator);
 
-function updateStops(mtl, stops) {
-  let projection = d3.geoMercator().fitSize([2400, 1200], mtl);
-  let geoGenerator = d3.geoPath().projection(projection);
-  let svg = d3
-    .select("#stops g.circles")
+  let arrets = d3
+    .select("#content g.stops")
     .selectAll("circle")
     .data(stops.features)
     .join("circle");
-
-  svg
+  arrets
     .attr("cx", function (d) {
-      console.log("here");
-      return projection(+d.geometry.coordinates)[0];
+      return projection(d.geometry.coordinates)[0];
     })
     .attr("cy", function (d) {
-      console.log("here");
-      return projection(+d.geometry.coordinates)[1];
+      return projection(d.geometry.coordinates)[1];
     })
     .attr("fill", "yellow")
     .style("opacity", 1) // Adjust the opacity as needed
-    .attr("r", 4);
+    .attr("r", 1);
+
+  let lignes = d3
+    .select("#content g.lines")
+    .selectAll("path")
+    .data(lines.features);
+  lignes.join("path").attr("d", geoGenerator);
 }
 
-Promise.all([d3.json("data/geobase.json"), d3.json("data/stm.geojson")]).then(
-  function (data) {
-    console.log(data[0]);
-    mtl = data[0];
-    console.log(data[1]);
-    stops = data[1];
-    updateMap(mtl);
-    updateStops(mtl, stops);
-  }
-);
+Promise.all([
+  d3.json("data/geobase.json"),
+  d3.json("data/stm.geojson"),
+  d3.json("data/stm_lines.geojson"),
+]).then(function (data) {
+  mtl = data[0];
+  stops = data[1];
+  lines = data[2];
+  updateMap(mtl, stops, lines);
+});
