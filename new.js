beb203c50a7dd1ef8871ea8f09dbf256ae05cd8a
Finn Kleckner <finn.kleckner@gmail.com>
2024-02-26 14:02:05 -0500

    CKUT website style

--- new.js
+++ new.js
@@ -8,10 +8,9 @@ function attachToolTip() {
     .on("mouseover", function (e, d) {
       tip
         .style("opacity", 1)
-        .style("left", e.pageX + "px")
-        .style("top", e.pageY + "px")
+        .style("left", "70%")
+        .style("top", "20%")
         .html(d.properties.name);
-      this.style("fill", "#7F00FF");
     })
     .on("mouseout", function (d) {
       tip.style("opacity", 0);
@@ -19,23 +18,48 @@ function attachToolTip() {
 }
 
 function updateMap(mtl, streets) {
-  console.log(mtl.features.length);
   let projection = d3.geoMercator().fitSize([2400, 1200], mtl);
   let path = d3.geoPath().projection(projection);
 
   let map = d3.select("#content g.map").selectAll("path").data(mtl.features);
 
-  let lignes = d3
-    .select("#content g.lines")
-    .selectAll("path")
-    .data(streets.features);
-  lignes.join("path").attr("d", path).style("stroke", "#000");
-  map
-    .join("path")
-    .attr("d", path)
-    .style("fill", "#999")
-    .style("stroke", "#000");
+  // let lignes = d3
+  //   .select("#content g.lines")
+  //   .selectAll("path")
+  //   .data(streets.features);
+  // lignes.join("path").attr("d", path).style("stroke", "#000");
+  map.join("path").attr("d", path);
+  // .style("fill", "#999")
+  // .style("stroke", "#000");
 
+  let loc = [-73.57954, 45.5081];
+  var sym = d3.symbol().type(d3.symbolStar).size(300);
+  let star = d3
+    .select("#content g.stops")
+    .append("path")
+    .attr(
+      "transform",
+      "translate(" + projection(loc)[0] + "," + projection(loc)[1] + ")"
+    );
+
+  star.attr("d", sym).attr("fill", "#FFD700");
+
+  var starTip = d3
+    .select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+  d3.select("#content g.stops")
+    .on("mouseover", function (e, d) {
+      starTip
+        .style("opacity", 1)
+        .style("left", e.pageX + "px")
+        .style("top", e.pageY + "px")
+        .html("CKUT Station!");
+    })
+    .on("mouseout", function (d) {
+      starTip.style("opacity", 0);
+    });
   attachToolTip();
 }
 
